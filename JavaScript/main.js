e863ff9bf8f4b7249eba971df40260e21ee0bed1
Adedotun Gabriel <adedotungabriel20@gmail.com>
2022-09-15 13:31:51 +0100

    well, figured out, i had an old code that couldn't run due to nul, and it's breaking my JavaScript, but It's fixed now, everything working fine ☺️, up next, GSAP!!!

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,15 +1,7 @@
-document.querySelector("video").playbackRate = 0.5;
-
 const nav = document.querySelector("nav");
 const menu = document.querySelector(".menu");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("open");
   nav.classList.toggle("open");
-
-  console.log("clicked");
-});
-
-document.addEventListener("click", (e) => {
-  console.log(e.target);
 });
