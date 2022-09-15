9ead98ec9976ab87253f8940cfe08573f1bc0be3
Adedotun Gabriel <adedotungabriel20@gmail.com>
2022-09-15 13:26:50 +0100

    well, I've done the last of thw work, but my javascript seems not to be working 😑🥲

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,15 @@
 document.querySelector("video").playbackRate = 0.5;
+
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  nav.classList.toggle("open");
+
+  console.log("clicked");
+});
+
+document.addEventListener("click", (e) => {
+  console.log(e.target);
+});
